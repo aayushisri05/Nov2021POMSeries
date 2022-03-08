981765a5047444444e73252b830014a32cba1163
aayushisri05 <aayushisri05@gmail.com>
2022-03-08 22:08:08 +0530

    initial code for ui framework

---
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Feature {
+	
+	public void f1() {
+		System.out.println("f1");
+	}
+	
+	public void f2() {
+		System.out.println("f2");
+	}
+
+}
---
+++ src/main/java/com/qa/opencart/pages/SearchPage.java
@@ -0,0 +1,19 @@
+package com.qa.opencart.pages;
+
+public class SearchPage {
+	
+	
+	public static void main(String a[]) {
+		
+		double d = 988762599d;
+		float f = 23.00f;
+		System.out.println(d+f);
+		
+		
+		
+	}
+	
+	
+	
+
+}
